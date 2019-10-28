34cebff84931cc26c6ec6328d12c5ba1ef26150b
Nastya <teplaya.1998@mail.ru>
2019-10-28 17:01:52 +0500

    Добавление загрузки

---
+++ Unity/Assets/load.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class load : MonoBehaviour
+{
+    public Slider slider;
+   private float timer;
+public int Level;
+private int slidV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       slider.value = slidV;
+timer +=Time.deltaTime;
+if (timer >= 2) {
+slidV += 7;
+timer =0;
+}
+if (slider.value >= 100) {
+            Application.LoadLevel(Level);
+        }
+    }
+}
