5a758510d7bd8cc75edc036020204e636f12caf8
Nastya <teplaya.1998@mail.ru>
2019-12-10 15:29:18 +0500

    Добавление персонажей на уровень, настройка движения главного героя, телепортация

---
+++ Unity/Assets/personag.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personag : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    public GameObject p1, p2, p3;
+
+    public float timer = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1.SetActive(true);
+        p2.SetActive(false);
+        p3.SetActive(false);
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        Vector2 moveV = new Vector2(Input.GetAxis("Horizontal") * 40, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = moveV;
+
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            p2.SetActive(true);
+            p1.SetActive(false);
+            p3.SetActive(false);
+        } else if (Input.GetAxis("Horizontal") > 0)
+        {
+            p3.SetActive(true);
+            p1.SetActive(false);
+            p2.SetActive(false);
+        } else
+        {
+            p1.SetActive(true);
+            p2.SetActive(false);
+            p3.SetActive(false);
+        }
+    }
+}
---
+++ Unity/Assets/xolod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xolod : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) anim.Play("open_xolod");
+    }
+
+}
